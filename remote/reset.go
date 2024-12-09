e7127e4bd6ed4b73ebbe63b8ff5baad6d7af4267
Jacalz <jacalz@tutanota.com>
2024-12-09 14:53:45 +0100

    Implement support for getting reset time

--- remote/reset.go
+++ remote/reset.go
@@ -19,7 +19,22 @@ func (c *Control) StopResetDelay() error {
 }
 
 // GetResetDelay returns the current delay until reset.
-func (c *Control) GetResetDelay() (uint8, error) {
+// Returns the delay or a bool indicating if it is stopped or not.
+func (c *Control) GetResetDelay() (uint8, bool, error) {
 	_, err := fmt.Fprintf(c.conn, commandFormat, "r", "?")
-	return 0, err
+
+	resp := [6]byte{}
+	n, err := c.conn.Read(resp[:])
+	if err != nil {
+		return 0, false, err
+	}
+
+	// Check if reset is stopped or not enabled.
+	if n == 4 && resp[3] == '~' {
+		return 0, true, nil
+	}
+
+	number := resp[3:n]
+	delay, err := strconv.ParseUint(string(number), 10, 8)
+	return uint8(delay), false, err
 }
