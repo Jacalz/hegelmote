d7f0a5a6652c3c6749e8177280f6d49834bea61d
Jacalz <jacalz@tutanota.com>
2025-02-26 15:40:03 +0100

    Format using gofumpt

--- main.go
+++ main.go
@@ -44,13 +44,15 @@ func buildRemoteUI(command *remote.Control, w fyne.Window) fyne.CanvasObject {
 	volume, err := command.GetVolume()
 	showErrorOnFailure(err, w)
 	volumeDisplay := &widget.Label{Text: strconv.Itoa(int(volume)) + "%"}
-	volumeSlider := &widget.Slider{Min: 0, Max: 100, Step: 1, Value: float64(volume), Orientation: widget.Horizontal,
+	volumeSlider := &widget.Slider{
+		Min: 0, Max: 100, Step: 1, Value: float64(volume), Orientation: widget.Horizontal,
 		OnChanged: func(f float64) {
 			volumeDisplay.SetText(strconv.Itoa(int(f)) + "%")
 		},
 		OnChangeEnded: func(f float64) {
 			showErrorOnFailure(command.SetVolume(uint8(f)), w)
-		}}
+		},
+	}
 
 	volumeMute := &widget.Button{Icon: theme.VolumeMuteIcon(), OnTapped: func() {
 		showErrorOnFailure(command.ToggleVolumeMute(), w)
