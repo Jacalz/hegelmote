3b8cb43239b28ea6c7ad50a7a9b2574855cd3f96
Jacalz <jacalz@tutanota.com>
2025-02-26 15:39:46 +0100

    Improve usability of POC gui

--- main.go
+++ main.go
@@ -1,31 +1,104 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/Jacalz/hegelmote/device"
 	"github.com/Jacalz/hegelmote/remote"
 )
 
-func buildUI(command *remote.Control) fyne.CanvasObject {
-	power := &widget.Button{Text: "Toggle Power", OnTapped: func() { command.TogglePower() }}
-	volumeMute := &widget.Button{Icon: theme.VolumeMuteIcon(), OnTapped: func() { command.ToggleVolumeMute() }}
-	volumeUp := &widget.Button{Icon: theme.VolumeUpIcon(), OnTapped: func() { command.VolumeUp() }}
-	volumeDown := &widget.Button{Icon: theme.VolumeDownIcon(), OnTapped: func() { command.VolumeDown() }}
+func showErrorOnFailure(err error, w fyne.Window) {
+	if err != nil {
+		dialog.ShowError(err, w)
+	}
+}
+
+func buildRemoteUI(command *remote.Control, w fyne.Window) fyne.CanvasObject {
+	power := &widget.Button{}
+	power.OnTapped = func() {
+		showErrorOnFailure(command.TogglePower(), w)
+
+		on, err := command.GetPower()
+		showErrorOnFailure(err, w)
+		if on {
+			power.SetText("Power off")
+		} else {
+			power.SetText("Power on")
+		}
+	}
+
+	on, err := command.GetPower()
+	showErrorOnFailure(err, w)
+	if on {
+		power.Text = "Power off"
+	} else {
+		power.Text = "Power on"
+	}
+
+	volume, err := command.GetVolume()
+	showErrorOnFailure(err, w)
+	volumeDisplay := &widget.Label{Text: strconv.Itoa(int(volume)) + "%"}
+	volumeSlider := &widget.Slider{Min: 0, Max: 100, Step: 1, Value: float64(volume), Orientation: widget.Horizontal,
+		OnChanged: func(f float64) {
+			volumeDisplay.SetText(strconv.Itoa(int(f)) + "%")
+		},
+		OnChangeEnded: func(f float64) {
+			showErrorOnFailure(command.SetVolume(uint8(f)), w)
+		}}
+
+	volumeMute := &widget.Button{Icon: theme.VolumeMuteIcon(), OnTapped: func() {
+		showErrorOnFailure(command.ToggleVolumeMute(), w)
+
+		muted, err := command.GetVolumeMute()
+		showErrorOnFailure(err, w)
+		if muted {
+			volumeSlider.SetValue(0)
+			return
+		}
+
+		volume, err := command.GetVolume()
+		showErrorOnFailure(err, w)
+		volumeSlider.SetValue(float64(volume))
+	}}
+	volumeUp := &widget.Button{Icon: theme.VolumeUpIcon(), OnTapped: func() {
+		showErrorOnFailure(command.VolumeUp(), w)
+
+		volume, err := command.GetVolume()
+		showErrorOnFailure(err, w)
+		volumeSlider.SetValue(float64(volume))
+	}}
+	volumeDown := &widget.Button{Icon: theme.VolumeDownIcon(), OnTapped: func() {
+		showErrorOnFailure(command.VolumeDown(), w)
+
+		volume, err := command.GetVolume()
+		showErrorOnFailure(err, w)
+		volumeSlider.SetValue(float64(volume))
+	}}
+
+	inputLabel := &widget.Label{Text: "Select input:", TextStyle: fyne.TextStyle{Bold: true}}
 
 	deviceType := device.H95
-	inputs, _ := device.GetInputNames(deviceType)
-	inputSelector := &widget.Select{Options: inputs, PlaceHolder: "Select input to use", OnChanged: func(input string) { command.SetSourceName(deviceType, input) }}
-
-	source, _ := command.GetSourceName(device.H95)
-	inputSelector.Selected = source
+	inputs, err := device.GetInputNames(deviceType)
+	showErrorOnFailure(err, w)
+	source, err := command.GetSourceName(deviceType)
+	showErrorOnFailure(err, w)
+	inputSelector := &widget.Select{Options: inputs, OnChanged: func(input string) { showErrorOnFailure(command.SetSourceName(deviceType, input), w) }, Selected: source}
 
 	return container.NewVBox(
 		power,
-		container.NewGridWithColumns(3, volumeMute, volumeDown, volumeUp),
+		widget.NewSeparator(),
+		container.NewVBox(
+			container.NewBorder(nil, nil, nil, volumeDisplay, volumeSlider),
+			container.NewGridWithColumns(3, volumeMute, volumeDown, volumeUp),
+		),
+		widget.NewSeparator(),
+		inputLabel,
 		inputSelector,
 	)
 }
@@ -42,7 +115,7 @@ func main() {
 		panic(err)
 	}
 
-	w.SetContent(buildUI(command))
+	w.SetContent(buildRemoteUI(command, w))
 	w.Resize(fyne.NewSize(300, 400))
 	w.SetMaster()
 	w.ShowAndRun()
