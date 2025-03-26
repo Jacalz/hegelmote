fd08ed6ac04d760a6d44fd039012fd87f2ceed97
Jacalz <jacalz@tutanota.com>
2025-03-26 21:27:24 +0100

    Clean up contents of main.go

--- main.go
+++ main.go
@@ -1,163 +1,17 @@
 package main
 
 import (
-	_ "embed"
-	"strconv"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 	"github.com/Jacalz/hegelmote/device"
 	"github.com/Jacalz/hegelmote/remote"
 )
-
-type disableableWidget interface {
-	fyne.Widget
-	fyne.Disableable
-}
-
-func enableAndRefresh(wid disableableWidget) {
-	if !wid.Disabled() {
-		wid.Refresh()
-	}
-	wid.Enable()
-}
-
-func disableAndRefresh(wid disableableWidget) {
-	if wid.Disabled() {
-		wid.Refresh()
-	}
-	wid.Disable()
-}
-
-type remoteUI struct {
-	amplifier state
-	window    fyne.Window
-
-	// Widgets:
-	powerToggle                      *widget.Button
-	volumeDisplay                    *widget.Label
-	volumeSlider                     *widget.Slider
-	volumeMute, volumeDown, volumeUp *widget.Button
-	inputSelector                    *widget.Select
-}
-
-func (r *remoteUI) syncState() {
-	r.volumeSlider.OnChangeEnded = nil
-	r.inputSelector.OnChanged = nil
-
-	// Input:
-	r.inputSelector.Selected = r.amplifier.input
-
-	// Power:
-	if r.amplifier.poweredOn {
-		r.powerToggle.SetText("Power off")
-		r.volumeMute.Enable()
-		r.volumeDown.Enable()
-		r.volumeUp.Enable()
-		enableAndRefresh(r.inputSelector)
-	} else {
-		r.powerToggle.SetText("Power on")
-		r.volumeMute.Disable()
-		r.volumeDown.Disable()
-		r.volumeUp.Disable()
-		disableAndRefresh(r.inputSelector)
-	}
-
-	// Volume:
-	r.volumeSlider.Value = float64(r.amplifier.volume)
-	r.volumeSlider.OnChanged(r.volumeSlider.Value)
-
-	// Mute:
-	if r.amplifier.muted || !r.amplifier.poweredOn {
-		disableAndRefresh(r.volumeSlider)
-	} else {
-		enableAndRefresh(r.volumeSlider)
-	}
-
-	r.inputSelector.OnChanged = r.onInputSelect
-	r.volumeSlider.OnChangeEnded = r.onVolumeDragEnd
-}
-
-func (r *remoteUI) onPowerToggle() {
-	r.amplifier.togglePower()
-	r.syncState()
-}
-
-func (r *remoteUI) onVolumeDrag(percentage float64) {
-	r.volumeDisplay.SetText(strconv.Itoa(int(percentage)) + "%")
-}
-
-func (r *remoteUI) onVolumeDragEnd(percentage float64) {
-	r.amplifier.setVolume(uint8(percentage))
-	r.syncState()
-}
-
-func (r *remoteUI) onMute() {
-	r.amplifier.toggleMute()
-	r.syncState()
-}
-
-func (r *remoteUI) onVolumeDown() {
-	r.amplifier.volumeDown()
-	r.syncState()
-}
-
-func (r *remoteUI) onVolumeUp() {
-	r.amplifier.volumeUp()
-	r.syncState()
-}
-
-func (r *remoteUI) onInputSelect(input string) {
-	r.amplifier.setInput(input)
-	r.syncState()
-}
-
-func buildRemoteUI(command *remote.Control, w fyne.Window) (*remoteUI, fyne.CanvasObject) {
-	ui := &remoteUI{window: w, amplifier: state{control: command}}
-	ui.amplifier.load()
-
-	ui.powerToggle = &widget.Button{Text: "Toggle power", OnTapped: ui.onPowerToggle}
-
-	ui.volumeDisplay = &widget.Label{Text: "0%"}
-	ui.volumeSlider = &widget.Slider{Min: 0, Max: 100, Step: 1, OnChanged: ui.onVolumeDrag, OnChangeEnded: ui.onVolumeDragEnd}
-
-	ui.volumeMute = &widget.Button{Icon: theme.VolumeMuteIcon(), OnTapped: ui.onMute}
-
-	ui.volumeDown = &widget.Button{Icon: theme.VolumeDownIcon(), OnTapped: ui.onVolumeDown}
-	ui.volumeUp = &widget.Button{Icon: theme.VolumeUpIcon(), OnTapped: ui.onVolumeUp}
-
-	inputLabel := &widget.Label{Text: "Select input:", TextStyle: fyne.TextStyle{Bold: true}}
-
-	inputs, _ := device.GetInputNames(ui.amplifier.control.Model) // TODO: Move this to a connection step.
-	ui.inputSelector = &widget.Select{Options: inputs, PlaceHolder: "Select an input", OnChanged: ui.onInputSelect}
-
-	ui.amplifier.load()
-	ui.syncState()
-
-	ui.amplifier.listenForChanges(func() { fyne.Do(ui.syncState) })
-
-	return ui, container.NewVBox(
-		ui.powerToggle,
-		widget.NewSeparator(),
-		container.NewVBox(
-			container.NewBorder(nil, nil, nil, ui.volumeDisplay, ui.volumeSlider),
-			container.NewGridWithColumns(3, ui.volumeMute, ui.volumeDown, ui.volumeUp),
-		),
-		widget.NewSeparator(),
-		inputLabel,
-		ui.inputSelector,
-	)
-}
 
 func main() {
 	a := app.NewWithID("io.github.jacalz.hegelmote")
 	w := a.NewWindow("Hegelmote")
 
 	command := &remote.Control{}
-
 	err := command.Connect("192.168.1.251:50001", device.H95)
 	if err != nil {
 		panic(err)
