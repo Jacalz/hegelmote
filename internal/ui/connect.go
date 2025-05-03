3da8ef94dc4333ace5f2ae069cda5d1b0e06e3d1
Jacalz <jacalz@tutanota.com>
2025-05-03 21:14:25 +0200

    Move info.go into connect.go

--- internal/ui/connect.go
+++ internal/ui/connect.go
@@ -9,6 +9,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/Jacalz/hegelmote/device"
 	"github.com/Jacalz/hegelmote/internal/upnp"
@@ -198,4 +199,40 @@ func showConnectionDialog(ui *mainUI, w fyne.Window) {
 
 		selectFromOneDevice(devices[0], ui, w)
 	}()
+}
+
+func (m *mainUI) onConnectionInfo() {
+	info := &widget.Form{Items: []*widget.FormItem{
+		{Text: "Address", Widget: &widget.Label{Text: m.host}},
+		{Text: "Model", Widget: &widget.Label{Text: "Hegel " + device.SupportedTypeNames()[m.amplifier.GetDeviceType()]}},
+		{Text: "Status", Widget: &widget.Label{Text: m.connectionLabel.Text}},
+	}}
+
+	prefs := fyne.CurrentApp().Preferences()
+	var infoDialog *dialog.CustomDialog
+
+	disconnect := &widget.Button{Text: "Disconnect", Icon: theme.CancelIcon(), Importance: widget.LowImportance, OnTapped: func() {
+		infoDialog.Hide()
+		m.Disconnect()
+		showConnectionDialog(m, m.window)
+	}}
+
+	forget := &widget.Button{Text: "Forget", Icon: theme.MediaReplayIcon(), Importance: widget.LowImportance}
+	forget.OnTapped = func() {
+		forget.Disable()
+		prefs.RemoveValue("host")
+		prefs.RemoveValue("model")
+	}
+
+	host := prefs.String("host")
+	modelID := prefs.IntWithFallback("model", -1)
+	if host == "" || modelID == -1 {
+		forget.Disable()
+	}
+
+	prop := &canvas.Rectangle{}
+	prop.SetMinSize(fyne.NewSquareSize(theme.Padding()))
+
+	infoDialog = dialog.NewCustom("Connection info", "Dismiss", container.NewVBox(info, container.NewGridWithRows(1, disconnect, forget), prop), m.window)
+	infoDialog.Show()
 }
