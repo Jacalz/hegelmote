c5c9a65ea44bd7a8e910ad57f526c7b720039487
Jacalz <jacalz@tutanota.com>
2025-05-05 22:33:14 +0200

    Revert "Bundle icon into app"
    This reverts commit fef3c699e48e265eb0698ac44a50dcda6b7f99e5.
    Not actually needed.

--- assets/img/img.go
+++ assets/img/img.go
@@ -13,13 +13,4 @@ var (
 
 	// PowerIcon is a fyne resource used to indicate a power on/off button.
 	PowerIcon = theme.NewThemedResource(&fyne.StaticResource{StaticName: "power.svg", StaticContent: powerIconContents})
-
-	//go:embed icon-512.png
-	iconContents []byte
-
-	// AppIcon holds the application icon.
-	AppIcon = &fyne.StaticResource{
-		StaticName:    "icon.png",
-		StaticContent: iconContents,
-	}
 )
