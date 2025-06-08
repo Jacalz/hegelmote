9ce89aac9fe6c90c36916701d6f6bb57005f415e
Jacalz <jacalz@tutanota.com>
2025-06-08 21:50:46 +0200

    Allow running server without hosting WASM files
    For #31

--- cmd/webmote/main.go
+++ cmd/webmote/main.go
@@ -17,15 +17,26 @@ func main() {
 		defer stop()
 	}
 
-	http.Handle("/", http.FileServer(http.Dir("./wasm")))
+	port := uint64(8086)
+	flag.Uint64Var(&port, "port", port, "port to serve on")
+	noWASM := false
+	flag.BoolVar(&noWASM, "no-wasm", noWASM, "disable hosting of WASM files")
+	flag.Parse()
+
+	if args := flag.Args(); len(args) > 0 {
+		fmt.Printf("invalid arguments: %v\n", args)
+		flag.Usage()
+		return
+	}
+
+	if !noWASM {
+		http.Handle("/", http.FileServer(http.Dir("./wasm")))
+	}
 	http.Handle("/proxy", http.HandlerFunc(proxyHandler))
 	http.Handle("/upnp", http.HandlerFunc(upnpHandler))
 
-	port := uint64(8086)
-	flag.Uint64Var(&port, "port", port, "port to serve on")
-	flag.Parse()
-
 	portString := strconv.FormatUint(port, 10)
+
 	fmt.Printf("Serving at: http://localhost:%s\n", portString)
 
 	server := http.Server{Addr: ":" + portString, ReadTimeout: time.Second, WriteTimeout: time.Second}
